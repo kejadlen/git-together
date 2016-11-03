00f784d0f0311f46329057ba97c33ed70206bdcf
Alpha Chen <alpha.chen@gmail.com>
2016-11-03 15:42:43 -0700

    refactor config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, Output};
 use errors::*;
 
 pub trait Config {
@@ -10,30 +10,33 @@ pub struct GitConfig {
   pub namespace: String,
 }
 
+impl GitConfig {
+  fn output(&self, args: &[&str]) -> Result<Output> {
+    Command::new("git")
+      .arg("config")
+      .args(args)
+      .output()
+      .chain_err(|| "failed to execute `git config`")
+      .and_then(|output| {
+        if output.status.success() {
+          Ok(output)
+        } else {
+          Err(ErrorKind::GitConfig(output).into())
+        }
+      })
+  }
+}
+
 impl Config for GitConfig {
   fn get(&self, name: &str) -> Result<String> {
     let name = format!("{}.{}", self.namespace, name);
-    let output = try!(Command::new("git")
-      .args(&["config", &name])
-      .output()
-      .chain_err(|| "failed to execute `git config`"));
-
-    if !output.status.success() {
-      let err = format!("failed to execute `git config`: {}",
-                        String::from_utf8_lossy(&output.stderr).trim());
-      return Err(err.into());
-    }
-
+    let output = try!(self.output(&[&name]));
     let stdout = String::from_utf8_lossy(&output.stdout);
     Ok(stdout.trim().into())
   }
 
   fn set(&self, name: &str, value: &str) -> Result<()> {
     let name = format!("{}.{}", self.namespace, name);
-    try!(Command::new("git")
-      .args(&["config", &name, &value])
-      .status()
-      .chain_err(|| "failed to execute `git config`"));
-    Ok(())
+    self.output(&[&name, &value]).and(Ok(()))
   }
 }
--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,13 @@
-error_chain!{}
+use std::process::Output;
+
+error_chain! {
+  errors {
+    GitConfig(output: Output) {
+      description("git config error")
+      display("git config error ({:?}): '{}'",
+        output.status.code(),
+        String::from_utf8_lossy(&output.stderr).trim(),
+      )
+    }
+  }
+}
