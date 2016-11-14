d9f5f660d305a709b0f57774ed5113ee5e52509a
Alpha Chen <alpha.chen@gmail.com>
2016-11-13 16:52:02 -0800

    NamespacedConfig

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,32 @@ pub trait Config {
   fn set(&mut self, name: &str, value: &str) -> Result<()>;
 }
 
+pub struct NamespacedConfig<C> {
+  namespace: String,
+  config: C,
+}
+
+impl<C> NamespacedConfig<C> {
+  fn namespaced(&self, name: &str) -> String {
+    format!("{}.{}", self.namespace, name)
+  }
+}
+
+impl<C: Config> Config for NamespacedConfig<C> {
+  fn get(&self, name: &str) -> Result<String> {
+    self.config.get(&self.namespaced(name))
+  }
+
+  fn get_all(&self, glob: &str) -> Result<HashMap<String, String>> {
+    self.config.get_all(&self.namespaced(glob))
+  }
+
+  fn set(&mut self, name: &str, value: &str) -> Result<()> {
+    let name = self.namespaced(name);
+    self.config.set(&name, value)
+  }
+}
+
 pub struct GitConfig {
   repo: git2::Repository,
   config: git2::Config,
