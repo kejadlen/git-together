a4cf15d4f4b4952cc561f7c5ebc355e68852c004
Alpha Chen <alpha.chen@gmail.com>
2017-01-15 15:28:22 -0800

    don't cargo run?

--- ci/script.sh
+++ ci/script.sh
@@ -14,8 +14,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
 
     ~/bin/bats --tap bats
 }
