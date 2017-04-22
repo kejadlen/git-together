76031c12f1024e289bc05334323dbd540c21d1bf
Alpha Chen <alpha.chen@gmail.com>
2017-04-22 09:13:32 -0700

    reset

--- src/config.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-use errors::*;
-
-pub trait Config {
-    fn get(&self, name: &str) -> Result<String>;
-    fn get_all(&self, glob: &str) -> Result<HashMap<String, String>>;
-    fn add(&mut self, name: &str, value: &str) -> Result<()>;
-    fn set(&mut self, name: &str, value: &str) -> Result<()>;
-}
